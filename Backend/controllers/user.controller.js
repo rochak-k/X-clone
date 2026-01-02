0ac14373a7f1a488193cad8daffb3b36fba984eb
rochak-k <rochakkhatiwada@gmail.com>
2026-01-02 10:09:34 +0545

    Created and tested suggestedUser endpoint

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -38,7 +38,7 @@ export const followUnfollowUser = async(req,res) => {
         const newNotification = new Notification({
              type : 'follow',
             from : req.user._id,
-            to : usertoModify.id,
+            to : id,
         })
         await newNotification.save()
         res.status(200).json({message : "User followed successfully"})
@@ -50,5 +50,25 @@ export const followUnfollowUser = async(req,res) => {
 }
     }
     export const getSuggestedUser = async (req ,res ) =>{
-        
+   try {
+    const userId = req.user._id;
+    const usersFollowedByMe = await User.findById(userId).select("following")
+    const users = await User.aggregate([
+        {
+            $match : {_id : {$ne : userId}}
+        },
+        {
+            $sample : {size : 10}
+        }
+
+    ])
+   const filteredUsers = users.filter((user)=>!usersFollowedByMe.following.includes(user._id))
+   const suggestedUsers = filteredUsers.slice(0,4)
+   suggestedUsers.forEach((user)=> user.password = null)
+   res.status(200).json(suggestedUsers)
+   } catch (error) {
+    console.log("Error in getSuggestedUser", error.message)
+    res.status(500).json({error : "Internal server error"}) 
+   }
     }
+  
