35de04b63e576fd71e84ce56a7242fe84bcd9301
rochak-k <rochakkhatiwada@gmail.com>
2026-02-13 12:19:18 +0545

    Managed createPost and getSuggestedUsers functionality in frontend

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ cloudinary.config({
 });
 const app = express();
 const PORT = process.env.PORT 
-app.use(express.json());
+app.use(express.json({limit :"5mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use('/api/auth', authRoutes)
--- Frontend/src/components/common/Post.jsx
+++ Frontend/src/components/common/Post.jsx
@@ -5,19 +5,45 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import LoadingSpinner from "./LoadingSpinner";
+import toast from "react-hot-toast";
 
 const Post = ({ post }) => {
+  const queryClient = useQueryClient()
   const [comment, setComment] = useState("");
   const postOwner = post.user;
   const isLiked = false;
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const isMyPost = authUser._id === post.user._id;
 
-  const isMyPost = true;
-
+  const { mutate: deletePost, isPending } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/post/delete/${post._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Post deleted successfully");
+      queryClient.invalidateQueries({queryKey : ['posts']})
+    },
+  });
   const formattedDate = "1h";
 
   const isCommenting = false;
 
-  const handleDeletePost = () => {};
+  const handleDeletePost = () => {
+    deletePost();
+  };
 
   const handlePostComment = (e) => {
     e.preventDefault();
@@ -50,10 +76,13 @@ const Post = ({ post }) => {
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                <FaTrash
+                {!isPending && <FaTrash
                   className="cursor-pointer hover:text-red-500"
                   onClick={handleDeletePost}
-                />
+                />}
+                {isPending && (
+                  <LoadingSpinner size="sm"/>
+                )}
               </span>
             )}
           </div>
